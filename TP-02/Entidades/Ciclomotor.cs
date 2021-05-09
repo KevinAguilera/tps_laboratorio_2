c71772d6db4ed9b6ba9f5bb6a048e52f4b1203d6
KevinAguilera <67611246+KevinAguilera@users.noreply.github.com>
2021-05-09 10:53:59 -0300

    TP 2

---
+++ TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Clase derivada de Vehiculo
+    /// </summary>
+    public class Ciclomotor : Vehiculo
+    {
+        /// <summary>
+        /// Inicializa los atributos de Ciclomotor llamando al constructor de la clase base.
+        /// </summary>
+        /// <param name="marca">Marca con la que se inicializara el atributo</param>
+        /// <param name="chasis">Chasis con la que se inicializara el atributo</param>
+        /// <param name="color">Color con el que se inicializara el atributo</param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Los Ciclomotores son chicos
+        /// Sobrescribe el metodo heredado, el cual retornara el tamaño del Vehiculo
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return Vehiculo.ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Sobrescribe el metodo heredado
+        /// </summary>
+        /// <returns>Retorna un string con los datos del Vehiculo en cuestion</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
